7e5108354014ebea2dfb5e8336aff382e9759657
chloro <chloro@pop-os.fios-router.home>
2025-10-31 16:59:01 -0400

    Implement wipe functionality

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -43,7 +43,7 @@ export default function Home() {
       <section className="main-section">
         <div className="page-heading py-16">
           <h1>Track Your Applications & Resume Ratings</h1>
-          {!loadingResumes && resume?.length === 0 ? (
+          {!loadingResumes && resume?.length == 0 ? (
             <h2>No resumes found. Upload your first resume to get feedback.</h2>
           ):(
             <h2>Review your submissions and check AI-powered feedback.</h2>
